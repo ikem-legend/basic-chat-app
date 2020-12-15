f1519514a4c4ed576287120ea562a8ae10424bb1
Ikemefuna Obioha <ikemoobioha@gmail.com>
2020-12-15 11:12:27 +0100

    Initial project setup

---
+++ src/pages/Chat.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Chat = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="grid-rows-1">
+        <div className="grid-cols-1">Chat</div>
+      </div>
+    </div>
+  )
+}
+
+export default Chat
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="grid-rows-1">
+        <div className="grid-cols-1">Home</div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
