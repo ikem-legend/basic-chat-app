4a9cef75ebc3f7a3f21eff08e5643e1bd9522500
Ikemefuna Obioha <ikemoobioha@gmail.com>
2020-12-18 03:40:31 +0100

    Prettier setup

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,9 +3,9 @@ import "firebase/auth"
 import "firebase/database"
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FB_DATABASE_URL
 };
 
 firebase.initializeApp(config);
